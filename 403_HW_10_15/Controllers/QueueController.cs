e438b00776f7b26e7b8214940bd0c28db1580551
highlander336 <connorraymcleod@gmail.com>
2018-10-15 20:17:36 -0600

    Fixed the queue structure to match Ganderson.

--- 403_HW_10_15/Controllers/QueueController.cs
+++ 403_HW_10_15/Controllers/QueueController.cs
@@ -8,88 +8,91 @@ namespace _403_HW_10_15.Controllers
 {
     public class QueueController : Controller
     {
+        static Queue<string> theQueue = new Queue<string>();
+        string display = "";
         // GET: Queue
         public ActionResult Index()
         {
-            //does this work?
-            Contract.Ensures(Contract.Result<ActionResult>() != null);
-            Queue<string> theQueue = new Queue<string>();
-            //add to queue
-            void addToQueue(Queue<string> q)
-            {
-                int size = q.Count() + 1;
-                string addToQ = "New Entry " + size.ToString();
-                q.Enqueue(addToQ);
-            }
+            ViewBag.theQueue = theQueue;
+            return View("Index");
+        }
+
+        //add to queue
+        public ActionResult addToQueue()
+        {
+            int size = theQueue.Count() + 1;
+            string addToQ = "New Entry " + size.ToString();
+            theQueue.Enqueue(addToQ);
+            return View();
+        }
+        //remove everything from the queue
+        public ActionResult addListToQueue()
+        {
             //remove everything from the queue
-            void addListToQueue(Queue<string> qBig)
+            clearQueue();
+            //add 2000 to queue
+            for (int i = 2000; i > 0; i--)
             {
-                //remove everything from the queue
-                clearQueue(qBig);
-                //add 2000 to queue
-                for (int i = 2000; i > 0; i--)
-                {
-                    addToQueue(qBig);
-                }
-            }
-            //Display contents of queue
-            string displayQueue(Queue<string> qDisplay)
-            {
-                string display = "";
-                //error
-                if (!qDisplay.Any())
-                {
-                    display = "ERROR: There is nothing in the Queue, please add something.";
-                }
-                else
-                {
-                    foreach (string entry in qDisplay)
-                    {
-                        display = display + " " + entry;
-                    }
-                }
-                return display;
-            }
-            //delete any item from the queue
-            void deleteItem(Queue<string> qDelete)
-            {
-                string display = "";
-                if (!qDelete.Any())
-                {
-                    display = "ERROR: There is nothing in the Queue, please add something.";
-                }
-                else
-                {
-                    qDelete.Dequeue();
-                    display = "Last Item removed from queue.";
-                }
-            }
-            //Clear the Queue
-            void clearQueue(Queue<string> qClear)
-            {
-                qClear.Clear();
-            }
-            //search the Queue
-            string searchQueue(Queue<string> qSearch)
-            {
-                string display = "";
-                string searchString = "Entry";
-                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-                sw.Start();
-                if (qSearch.Contains(searchString))
-                {
-                    display = searchString + " was found in the queue!";
-                }
-                else
-                {
-                    display = searchString + " was not found in the queue!";
-                }
-                sw.Stop();
-                TimeSpan ts = sw.Elapsed;
-                ViewBag.StopWatch = ts;
-                return display;
+                addToQueue();
             }
             return View();
+        }
+        //Display contents of queue
+        public ActionResult displayQueue()
+        {
+            //error
+            if (!theQueue.Any())
+            {
+                display = "ERROR: There is nothing in the Queue, please add something.";
+            }
+            else
+            {
+                foreach (string entry in theQueue)
+                {
+                    display = display + " " + entry;
+                }
+            }
+            ViewBag.display = display;
+            return View("Index");
+        }
+        //delete any item from the queue
+        public ActionResult deleteItem()
+        {
+            if (!theQueue.Any())
+            {
+                display = "ERROR: There is nothing in the Queue, please add something.";
+            }
+            else
+            {
+                theQueue.Dequeue();
+                display = "Last Item removed from queue.";
+            }
+            return View();
+        }
+        //Clear the Queue
+        public ActionResult clearQueue()
+        {
+            theQueue.Clear();
+            return View();
+        }
+        //search the Queue
+        public ActionResult searchQueue()
+        {
+            string searchString = "Entry";
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            if (theQueue.Contains(searchString))
+            {
+                display = searchString + " was found in the queue!";
+            }
+            else
+            {
+                display = searchString + " was not found in the queue!";
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            ViewBag.StopWatch = ts;
+            return View("Index");
         }
     }
 }
