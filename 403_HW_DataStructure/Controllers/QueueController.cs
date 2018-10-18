2ce0b875bcb7577fd89a3869bdff2d5e29cd56da
Hoyong Jung <jhoyong57@gmail.com>
2018-10-18 11:13:56 -0600

    Maybe it works!

---
+++ 403_HW_DataStructure/Controllers/QueueController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace IamGonnaMakeItWork.Controllers
+{
+    public class QueueController : Controller
+    {
+        static Queue<string> theQueue = new Queue<string>();
+        string display = "Success";
+        // GET: Queue
+        public ActionResult Index()
+        {
+            //ViewBag.theQueue = theQueue;
+            return View();
+        }
+
+        //add to queue
+        public ActionResult addToQueue()
+        {
+            int size = theQueue.Count() + 1;
+            string addToQ = "New Entry " + size.ToString();
+            theQueue.Enqueue(addToQ);
+            ViewBag.theQueue = display;
+            return View("Index");
+        }
+
+        //add a list of 2000 to the queue
+        public ActionResult addListToQueue()
+        {
+            //remove everything from the queue
+            clearQueue();
+            //add 2000 to queue
+            for (int i = 2000; i > 0; i--)
+            {
+                addToQueue();
+            }
+            ViewBag.theQueue = "HUGE!";
+            return View("Index");
+        }
+
+        //Display contents of queue
+        public ActionResult displayQueue()
+        {
+            //error message
+            if (!theQueue.Any())
+            {
+                display = "ERROR: There is nothing in the Queue, please add something.";
+            }
+            else
+            {
+                display = "";
+                foreach (string entry in theQueue)
+                {
+
+                    display += entry + " ";
+                }
+            }
+            ViewBag.theQueue = display;
+            return View("Index");
+        }
+        //delete any item from the queue
+        public ActionResult deleteItem()
+        {
+            if (!theQueue.Any())
+            {
+                display = "ERROR: There is nothing in the Queue, please add something.";
+            }
+            else
+            {
+                theQueue.Dequeue();
+                display = "First Item removed from queue.";
+            }
+            ViewBag.theQueue = display;
+            return View("Index");
+        }
+        //Clear the Queue
+        public ActionResult clearQueue()
+        {
+            theQueue.Clear();
+            ViewBag.theQueue = "Cleared";
+            return View("Index");
+        }
+        //search the Queue
+        public ActionResult searchQueue()
+        {
+            string searchString = "Entry";
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            if (theQueue.Contains(searchString))
+            {
+                display = searchString + " was found in the queue!";
+            }
+            else
+            {
+                display = searchString + " was not found in the queue!";
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            ViewBag.StopWatch = ts;
+            ViewBag.theQueue = display;
+            return View("Index");
+        }
+        public ActionResult Menu()
+        {
+            theQueue.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ 403_HW_DataStructure/Controllers/StackController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IamGonnaMakeItWork.Controllers
+{
+    public class StackController : Controller
+    {
+        static Stack<string> MyStack = new Stack<string>();
+        string msg = "Success";
+        // GET: Stack
+        public ActionResult Index()
+        {
+            //ViewBag.MyStack = MyStack;
+            return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            int size = MyStack.Count() + 1;
+            string addStack= "New Entry " + size.ToString();
+            MyStack.Push(addStack);
+            ViewBag.MyStack = msg;
+            return View("Index");
+        }
+
+        public ActionResult AddHuge()
+        {
+            MyStack.Clear();
+
+            for (int i = 2000; i > 0; i--)
+            {
+                AddOne();
+            }
+            ViewBag.MyStack = "HUGE!";
+            return View("Index");
+        }
+
+        public ActionResult Display()
+        {
+            if (!MyStack.Any())
+            {
+                msg = "ERROR: There is nothing in the Stack, please add something.";
+            }
+            else
+            {
+                msg = "";
+                foreach (string entry in MyStack)
+                {
+
+                    msg += entry + " ";
+                }
+            }
+            ViewBag.MyStack = msg;
+            return View("Index");
+        }
+
+        public ActionResult Delete()
+        {
+            if (!MyStack.Any())
+            {
+                msg = "ERROR: There is nothing in the Stack, please add something.";
+            }
+            else
+            {
+                MyStack.Pop();
+                msg = "Last Item removed from stack.";
+            }
+            ViewBag.theQueue = msg;
+            return View("Index");
+        }
+
+        public ActionResult Clear()
+        {
+            MyStack.Clear();
+            ViewBag.MyStack = "Cleared";
+            return View("Index");
+        }
+
+        public ActionResult Search()
+        {
+            string searchString = "Entry";
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            if (MyStack.Contains(searchString))
+            {
+                msg = searchString + " was found in the stack!";
+            }
+            else
+            {
+                msg = searchString + " was not found in the stack!";
+            }
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            ViewBag.StopWatch = ts;
+            ViewBag.theQueue = msg;
+            return View("Index");
+        }
+
+        public ActionResult Menu()
+        {
+            MyStack.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
